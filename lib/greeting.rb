298662796842da082d2b2388eddc6094351e8c64
Rick Rosetti <rick.rosetti@gmail.com>
2019-03-30 03:49:06 +0000

    Automatically backed up by Learn

--- lib/greeting.rb
+++ lib/greeting.rb
@@ -1,7 +1,5 @@
 # code the #greeting method here!
-def greeting
-  puts "Hi! I'm HAL, what's your name?"
-  name = gets.strip
+def greeting(name)
   puts "Hello #{name}. It's nice to meet you."
 end
 
